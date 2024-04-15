0375f4038ed5763cd96e869adc63f1108220b152
Gith0109 <125884426+Gith0109@users.noreply.github.com>
2024-04-15 10:15:12 -0300

    Questao3.java

--- src/Questao3.java
+++ src/Questao3.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Questao3 {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         // Informações/Strings
 
-        int n1, n2, n3, n4 ;
+        int n1, n2, n3, n4;
 
         //Imprimindo e Armanezando dados
         Scanner total = new Scanner(System.in);
@@ -22,22 +22,13 @@ public class Questao3 {
         System.out.println("Calcular");
         n4 = total.nextInt();
 
-        //Fórmula da soma de Strings e Resultado
-
-        MediaAritmetica(n1, n2, n3, n4);
-    }
-
-    private static void MediaAritmetica(int n1, int n2, int n3, int n4) {
-
-
-
-        int mod, resultado ;
+        int mod, resultado;
         float divisao;
         resultado = n1 + n2 + n3 + n4;
         divisao = (float) resultado / 4;
         mod = resultado % 4;
-        System.out.printf("Resultado: %.2f (%d/%d)\n",divisao,resultado,4);
+
+        System.out.printf("Resultado: %.2f (%d/%d)\n", divisao, resultado, 4);
         System.out.printf("Resto de: %d", mod);
     }
 }
-
